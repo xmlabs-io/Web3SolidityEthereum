5ea8703829162f3d1c77a99f37dab86d646410a7
Xuan Mai Ho <exemaitch@users.noreply.github.com>
2021-12-25 15:42:45 +0000

    Added the ability to get total waves and to get all the waves.

--- scripts/run.js
+++ scripts/run.js
@@ -1,20 +1,29 @@
 
 
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
-  const waveContract = await waveContractFactory.deploy(owner.address);
+  const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
   console.log("Contract deployed to: ", waveContract.address);
-  console.log("Contract deployed by:", owner.address);
 
   let waveCount;
   waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
 
-  let waveTxn = await waveContract.wave();
+  let waveTxn = await waveContract.wave("Hiya!");
   await waveTxn.wait();
-  
+
+  const [_, randomPerson] = await hre.ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave('Another message!');
+  await waveTxn.wait();
+
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
   waveCount = await waveContract.getTotalWaves();
+
+  console.log(waveCount.toNumber());
+
+  
 };
 
 const runMain = async () => {
